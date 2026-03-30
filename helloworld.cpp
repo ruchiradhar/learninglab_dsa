031812cccd1adef2de736ec44d190b7c1390744b
Ruchira Dhar <ruchiradhar09@gmail.com>
2026-03-30 06:17:55 +0200

    Hello World in CPP

---
+++ helloworld.cpp
@@ -0,0 +1,18 @@
+// Preprocessor Directive as header file
+// # include tells compiler to include something in source code
+// iostream is a library containg C++ input and output functions
+// if header file has .h extension, can be used by other C programs
+# include <iostream> 
+
+// Namespace Declaration
+// it is used to group logically related functions and to avoid name conflicts
+using namespace std;
+
+// Name Function
+// main is the entry point of a C++ program
+// this code will be executed first when the program runs
+int main(){
+    // This statement prints "Hello, World!" to the console
+    cout << "Hello, World!"; //cout is the output statement in C++
+    return 0; //return indicates the exit status where 0 is success
+}
