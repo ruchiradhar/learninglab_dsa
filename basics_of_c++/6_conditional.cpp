ad1ff6fa92ba5f369e1de11f7fc3315744fb4918
Ruchira Dhar <ruchiradhar09@gmail.com>
2026-04-01 20:52:04 +0200

    Basics[Conditionals]: Learning if-else block

---
+++ basics_of_c++/6_conditional.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char n=45;
+    if (n>=0){
+        cout<<"n is positive"<<endl;
+    }
+    else{
+        cout<<"n is negative";
+    }
+    return 0;
+}
