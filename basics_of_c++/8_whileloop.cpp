a090b32d09b5dcece0cf7ac0d3faae8d90448585
Ruchira Dhar <ruchiradhar09@gmail.com>
2026-04-02 07:04:56 +0200

    Basics[While Loop]: Learning while loop

---
+++ basics_of_c++/8_whileloop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+// print 1 to n using while loop
+// while loop syntax: while(condition){ body of loop }
+int main(){
+
+    int n=20;
+    int count=1;
+    while (count<=n){ 
+        cout<<count<<endl;
+        count++;
+
+    }
+
+    return 0;
+}
