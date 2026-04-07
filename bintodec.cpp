c0e252429363d95585f5167cc9e47fb2cd917999
Ruchira Dhar <ruchiradhar09@gmail.com>
2026-04-07 05:51:17 +0200

    Binary to Dec Code

--- bintodec.cpp
+++ bintodec.cpp
@@ -2,19 +2,25 @@
 using namespace std;
 
 // binary to decimal conversion
-int binToDec(int biNum)
+int binToDecimal(int biNum)
 {
-    int ans =0; // the variable to store the number
-    int power =1; // the power to multiply ans with for getting number
+    int ans = 0; // the variable to store the number
+    int power = 1; // the power to multiply ans with for getting number
 
     while (biNum>0){
-        int lastDigit= biNum%10;
+        int rem= biNum%10;
+        ans= ans+ (rem*power);
+        
         biNum=biNum/10;
-
-        ans= ans+ lastDigit*power;
         power= power*2;
     }
     
 
+    return ans;
+}
+
+int main()
+{
+    cout<<"The decimal of 10 is "<<binToDecimal(10)<<endl;
     return 0;
 }
