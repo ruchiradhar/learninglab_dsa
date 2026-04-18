c1c9fa54383d29a913aaba1b4d01374c137930d9
Ruchira Dhar <ruchiradhar09@gmail.com>
2026-04-18 05:42:43 +0200

    Patterns[IncreasingTriangles]:Adding character case

--- basic_patterns/4_increasingtriangles.cpp
+++ basic_patterns/4_increasingtriangles.cpp
@@ -14,8 +14,25 @@ void triangle_num(){
     }
 }
 
+// triangle pattern with characters
+void triangle_char(){
+
+    int n=4;
+    
+
+    for (int i=0;i<n;i++){
+        char ch='A';
+        for (int j=0;j<=i;j++){
+            cout<<ch;
+            ch+=1;
+        }
+        cout<<endl;
+    }
+}
+
 int main()
 {
     triangle_num();
+    triangle_char();
     return 0;
 }
